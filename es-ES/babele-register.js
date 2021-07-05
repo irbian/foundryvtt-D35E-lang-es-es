48635de31357957fece1161ea995f30f66319acf
Irbian <3071767+irbian@users.noreply.github.com>
2021-07-05 18:50:23 +0100

    Fixing cache bug (thanks Slash and riccisi)

--- es-ES/babele-register.js
+++ es-ES/babele-register.js
@@ -1,14 +1,36 @@
-Hooks.on('init', () => {
+import * as cache from "../../systems/D35E/module/cache.js";
 
-    if(typeof Babele !== 'undefined') {
+Hooks.on('init', async () => {
+
+    if (typeof Babele !== 'undefined') {
+
+        const classEntries = await fetch("./modules/translation-D35E-compendium-es/compendium/D35E.classes.json")
+            .then(response => response.json())
+            .then(json => json.entries);
+        const classTransformation = _.mapValues(classEntries, 'name');
+
         Babele.get().register({
             module: 'translation-D35E-compendium-es',
             lang: 'es',
             dir: 'compendium'
         });
 
-        Babele.get().registerConverters({
-            "nameWithOriginal": (original, translation) => { return original !== translation ? `${translation} (${original})` : translation; }
-        });   
+        Babele.get().registerConverters({            
+            "nameWithOriginal": (original, translation) => { return original !== translation ? `${translation} (${original})` : translation; },
+            "classNamesArray": (classesArray) => {
+                classesArray.map((classArray) => {
+                    classArray[0] = classTransformation[classArray[0]]
+                        ? classTransformation[classArray[0]]
+                        : classArray[0];
+                    return classArray;
+                });
+                return classesArray;
+            }
+        });
     }
+});
+
+Hooks.once('babele.ready', () => {
+    console.info('translation-D35E-compendium-es | All translated, lets rebuild cache');
+    cache.rebuildCache();
 });
